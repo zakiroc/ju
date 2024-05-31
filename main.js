f39df397fcb81a46d264a571f1f7ef4f79a5be0d
zakiroc <164227048+zakiroc@users.noreply.github.com>
2024-05-31 23:35:33 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let namer = prompt('ismingizni kiriting')
+ 
+ let word =  prompt('bironta harf kiriting')
+
+
+ if(namer.includes(word)){
+    alert(`${namer} ismda ${word} harf mavjud`)
+ }
+ else{
+    alert('bu ismda yozgan harifiz mavjud emas')
+ }
+    
+ 
